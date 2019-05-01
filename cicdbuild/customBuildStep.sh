1ab68c5b772b7fcb09d1022fa0479195bf53de31
Julian Atkinson <jpa458@gmail.com>
2019-05-01 21:09:20 +0200

    parameterise the project name

--- cicdbuild/customBuildStep.sh
+++ cicdbuild/customBuildStep.sh
@@ -5,7 +5,7 @@ docker-credential-gcr configure-docker
 
 docker network create mynet
 
-docker run -d --name myserver --net mynet gcr.io/jpa-cicd-custombuild/hello-node-server:latest
+docker run -d --name myserver --net mynet gcr.io/$1/hello-node-server:latest
 
-docker run --net mynet gcr.io/jpa-cicd-custombuild/hello-node-client:latest -s http://myserver:8080
+docker run --net mynet gcr.io/$1/hello-node-client:latest -s http://myserver:8080
 
